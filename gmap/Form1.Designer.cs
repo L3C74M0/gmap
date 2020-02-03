6241b5cec55afbc37a4536dcb491c26555eb7f2b
HappyFeed <alejomam2011@gmail.com>
2020-02-02 20:07:38 -0500

    collumnsDone

--- gmap/Form1.Designer.cs
+++ gmap/Form1.Designer.cs
@@ -29,6 +29,21 @@
         private void InitializeComponent()
         {
             this.gmap = new GMap.NET.WindowsForms.GMapControl();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxSalida = new System.Windows.Forms.TextBox();
+            this.textBoxLLegada = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxMes = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.buttonPrevPage = new System.Windows.Forms.Button();
+            this.buttonNextPag = new System.Windows.Forms.Button();
+            this.labelNpage = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // gmap
@@ -54,26 +69,164 @@
             this.gmap.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
             this.gmap.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
             this.gmap.ShowTileGridLines = false;
-            this.gmap.Size = new System.Drawing.Size(795, 447);
+            this.gmap.Size = new System.Drawing.Size(606, 496);
             this.gmap.TabIndex = 0;
             this.gmap.Zoom = 0D;
             this.gmap.Load += new System.EventHandler(this.gMapController_Load);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(639, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Salida";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(639, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "LLegada";
+            // 
+            // textBoxSalida
+            // 
+            this.textBoxSalida.Location = new System.Drawing.Point(642, 56);
+            this.textBoxSalida.Name = "textBoxSalida";
+            this.textBoxSalida.Size = new System.Drawing.Size(113, 20);
+            this.textBoxSalida.TabIndex = 3;
+            // 
+            // textBoxLLegada
+            // 
+            this.textBoxLLegada.Location = new System.Drawing.Point(642, 107);
+            this.textBoxLLegada.Name = "textBoxLLegada";
+            this.textBoxLLegada.Size = new System.Drawing.Size(113, 20);
+            this.textBoxLLegada.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBoxMes);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(634, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(248, 181);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Data";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(5, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Mes";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBoxMes
+            // 
+            this.textBoxMes.Location = new System.Drawing.Point(8, 147);
+            this.textBoxMes.Name = "textBoxMes";
+            this.textBoxMes.Size = new System.Drawing.Size(113, 20);
+            this.textBoxMes.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dataGridView1);
+            this.groupBox2.Controls.Add(this.labelNpage);
+            this.groupBox2.Controls.Add(this.buttonNextPag);
+            this.groupBox2.Controls.Add(this.buttonPrevPage);
+            this.groupBox2.Location = new System.Drawing.Point(634, 212);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(248, 284);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Flights";
+            // 
+            // buttonPrevPage
+            // 
+            this.buttonPrevPage.Location = new System.Drawing.Point(56, 255);
+            this.buttonPrevPage.Name = "buttonPrevPage";
+            this.buttonPrevPage.Size = new System.Drawing.Size(35, 23);
+            this.buttonPrevPage.TabIndex = 0;
+            this.buttonPrevPage.Text = "<";
+            this.buttonPrevPage.UseVisualStyleBackColor = true;
+            this.buttonPrevPage.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonNextPag
+            // 
+            this.buttonNextPag.Location = new System.Drawing.Point(160, 255);
+            this.buttonNextPag.Name = "buttonNextPag";
+            this.buttonNextPag.Size = new System.Drawing.Size(35, 23);
+            this.buttonNextPag.TabIndex = 1;
+            this.buttonNextPag.Text = ">";
+            this.buttonNextPag.UseVisualStyleBackColor = true;
+            // 
+            // labelNpage
+            // 
+            this.labelNpage.AutoSize = true;
+            this.labelNpage.Location = new System.Drawing.Point(120, 260);
+            this.labelNpage.Name = "labelNpage";
+            this.labelNpage.Size = new System.Drawing.Size(13, 13);
+            this.labelNpage.TabIndex = 2;
+            this.labelNpage.Text = "1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(2, 33);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(246, 216);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.selectFlight);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(915, 508);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.textBoxLLegada);
+            this.Controls.Add(this.textBoxSalida);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.gmap);
+            this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "Maps";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private GMap.NET.WindowsForms.GMapControl gmap;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxSalida;
+        private System.Windows.Forms.TextBox textBoxLLegada;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxMes;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button buttonPrevPage;
+        private System.Windows.Forms.Label labelNpage;
+        private System.Windows.Forms.Button buttonNextPag;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- gmap/Form1.cs
+++ gmap/Form1.cs
@@ -10,11 +10,23 @@ using System.Windows.Forms;
 
 namespace gmap {
     public partial class Form1 : Form {
+
+        DataTable dt;
         public Form1() {
             InitializeComponent();
+
         }
 
         private void gMapController_Load(object sender, EventArgs e) {
+            dt = new DataTable();
+            dt.Columns.Add(new DataColumn("Year",typeof(String)));
+            dt.Columns.Add(new DataColumn("Month", typeof(String)));
+            dt.Columns.Add(new DataColumn("Salida", typeof(String)));
+            dt.Columns.Add(new DataColumn("Llegada", typeof(String)));
+            dt.Rows.Add("2017","march","Cali","Bogota");
+            dataGridView1.DataSource = dt;
+            dataGridView1.Columns[2].Visible = false;
+            dataGridView1.Columns[3].Visible = false;
             gmap.DragButton = MouseButtons.Left;
             gmap.CanDragMap = true;
             gmap.MapProvider = GMap.NET.MapProviders.GoogleTerrainMapProvider.Instance;
@@ -24,6 +36,32 @@ namespace gmap {
             gmap.MaxZoom = 18;
             gmap.Zoom = 9;
             gmap.AutoScroll = true;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void selectFlight(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            int filaSeleccionada = e.RowIndex;
+            String salida = dataGridView1.Rows[filaSeleccionada].Cells[2].Value.ToString();
+            String llegada = dataGridView1.Rows[filaSeleccionada].Cells[3].Value.ToString();
+            String month = dataGridView1.Rows[filaSeleccionada].Cells[1].Value.ToString();
+            textBoxMes.Text = month;
+            textBoxLLegada.Text = llegada;
+            textBoxSalida.Text = salida;
         }
     }
 }
